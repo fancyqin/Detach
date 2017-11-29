481a05521ee6918687cda247e887519acdd4e9f3
qinfan <fancarter007@163.com>
2017-11-29 18:12:02 +0800

    aaa

--- NViewEngine.js
+++ NViewEngine.js
@@ -1,25 +1,35 @@
 const fs = require('fs');
 const path = require('path');
+const _ = require('lodash');
+
 
 const art = require('express-art-template');
 const pug = require('pug');
 const ejs = require('ejs');
 const dot = require('dot');
+const handlebars = require('handlebars');
 
-const NViewEngine = {
-    config:{
-        charset:'utf-8',
-        defaultEngine:'ejs',
-        ext:{
-            html:'ejs',
-            ejs:'ejs',
-            dot:'dot',
-            jst:'dot',
-            pug:'pug',
-            jade:'pug',
-            art:'art'
-        }
+const defaults = {
+    charset:'utf-8',
+    defaultEngine:'ejs',
+    ext:{
+        html:'ejs',
+        ejs:'ejs',
+        dot:'dot',
+        jst:'dot',
+        pug:'pug',
+        jade:'pug',
+        art:'art',
+        'handlebars':'handlebars'
     },
+    application:'vo',
+    path:'/vo/view'
+};
+
+class NViewEngine {
+    constructor(config){
+        this.config =  _.defaults(config,defaults);
+    }
     compile(tplType,str,data){
         let htmlString;
         if(!tplType||!str||!data){
@@ -39,33 +49,43 @@ const NViewEngine = {
             case 'art':
                 htmlString = art.template.render(str,data);
                 break;
+            case 'handlebars':
+                let template = handlebars.compile(str);
+                htmlString = template(data);
+                break;
             default:
                 htmlString = ejs.render(str,data);
                 break;
         }
         return htmlString;
-    },
+    }
     getFileStr(fileURI){
         try{
             return fs.readFileSync(fileURI, this.config.charset);
         }catch(e) {
             return '路径error，找不到文件'
         }
-    },
+    }
     getFileExtName(fileURI){
         try {
             return path.extname(fileURI).split('.')[1]
         }catch (e){
             return '路径error，文件扩展名错误'
         }
-    },
+    }
     compileByUri(fileURI,data){
-        const extName = this.getFileExtName(fileURI);
-        const type = this.config.ext[extName.toLowerCase()] || this.config.defaultEngine ;
         const str  = this.getFileStr(fileURI);
-        return this.compile(type,str,data);
+        const extName = this.getFileExtName(fileURI) || '';
+        const type = this.config.ext[extName.toLowerCase()] || this.config.defaultEngine ;
+        try {
+            return this.compile(type,str,data);
+        }catch (e){
+            return '模板渲染错误'
+        }
     }
 
-};
+}
+
+
 
 module.exports = NViewEngine;
---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+
+const NViewEngine = require('./NViewEngine.js');
+const engine = new NViewEngine({
+    defaultEngine:'dot'
+});
+
+const data = JSON.parse(fs.readFileSync(__dirname+'/data/repositories.json'));
+
+const engine_route = (route,file,name) => {
+    app.get(route, (req, res) => {
+        data.engine = name;
+        data.xss = '<script>alert(1)</script>';
+        res.send(engine.compileByUri(__dirname +'/template-war/'+file,data))
+    });
+};
+
+
+engine_route('/art','index.art','art-template');
+engine_route('/ejs','index.ejs','ejs');
+engine_route('/dot','index.jst','doT');
+engine_route('/pug','index.pug','pug');
+engine_route('/handlebars','index.handlebars','handlebars')
+
+engine_route('/test','index.ddd','test');
+
+app.listen(3000, ()=> {
+    console.log('listening at 3000');
+});
